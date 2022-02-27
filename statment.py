34a0b736b4379180b1e07f6a20e454d282a7baec
chilldogs14 <100097061+chilldogs14@users.noreply.github.com>
2022-02-28 09:52:50 +1300

    print statments

---
+++ statment.py
@@ -0,0 +1,12 @@
+#Print the chorus from your favourite  song in a single print statement, using  line breaks escapes.
+print("Take me down to the paradise city Where the grass is green and the girls are pretty Take me home oh, won't you please take me home Take me down to the paradise city Where the grass is green and the girls are pretty Take me home oh, won't you please take me home")
+print("\"Joey doesn't share food!\"")
+print("a linear sequence of characters, words, or other data.")
+print("integers are zero, positive or negative whole numbers without a fractional part and having unlimited precision")
+print("converts a number stored in a string or integer into a floating point number, or a number with a decimal point.")
+print(543/3)
+print(543%3)
+print(1000%9)
+print(434//10)
+print(3%2)
+print(5%5)
