d02aa65d34bb4b6ddb7f222cbee5176a974ba94b
chilldogs14 <100097061+chilldogs14@users.noreply.github.com>
2022-03-02 12:13:09 +1300

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,21 @@
+player_lives = 3
+player_name = "Jeff"
+player_bank = 1000
+player_number = 021 738 4652
+name = "Jason"
+age = 15
+year = 2022
+print(name)
+print(age)
+print(year)
+surname	 = "Ischia"
+school = "kings high"
+food = "chicken"
+pet = "Ari"
+choclate = 15
+player_score = 26
+high_score = 65
+laps = 3
+gun_ammo = 15
+car_gear = 6
+car speed = 297
