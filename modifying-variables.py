9350d9c19c2acfe290658000d54f2f66b40f7412
chilldogs14 <100097061+chilldogs14@users.noreply.github.com>
2022-03-02 12:12:46 +1300

    Create modifying-variables.py

---
+++ modifying-variables.py
@@ -0,0 +1,22 @@
+money = 200
+hat = 20
+top = 30
+pants = 15
+belt = 60
+shoes = 40
+print(money)
+print("Buying a hat")
+money = money-hat
+print("You now have the following amount left over, " + str(money))
+print("Buying a top")
+money = money-top
+print("You now have the following amount left over, " + str(money))
+print("Buying a pair of pants")
+money = money-pants
+print("You now have the following amount left over, " + str(money))
+print("Buying a belt")
+money = money-belt
+print("You now have the following amount left over, " + str(money))
+print("Buying a pair of shoes")
+money = money-shoes
+print("You now have the following amount left over, " + str(money))
